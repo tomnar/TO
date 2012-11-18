acbf8ac1e23ad1d42866c41ab9e17c990bd1090f
Tomnar <tomquast89@gmail.com>
2012-11-18 15:25:17 +0100

    added listing things in mainactivity

--- src/trendsonline/cast/li/MainActivity.java
+++ src/trendsonline/cast/li/MainActivity.java
@@ -4,7 +4,7 @@ import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 
 import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilderFactory; 
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -12,12 +12,18 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+import com.loopj.android.http.*;
+
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends Activity { 
+public class MainActivity extends ListActivity { 
 
 	ArrayList<Article> articles;
 
@@ -25,15 +31,21 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		RSSConnection rssc = new RSSConnection();
-		rssc.requestHTTPGET("http://trendsonline.dk/feed/", this); 
 		articles = new ArrayList<Article>();
+		Log.w("apP", "received");
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get("https://dl.dropbox.com/u/2440776/trendsonline.rss", new AsyncHttpResponseHandler() {
+		    @Override
+		    public void onSuccess(String response) {
+				httpGetFinished(response);
+		    }
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu); 
+		// Inflate the menu; this adds it ems to the action bar if it is present.  
+		getMenuInflater().inflate(R.menu.activity_main, menu);  
 		return true;
 	}
 
@@ -57,15 +69,30 @@ public class MainActivity extends Activity {
 				}
 			}
 
+			ArrayList<String> titles = new ArrayList<String>();
 			for (Article a : articles) {
-				Log.w("app", a.getTitle());
+				titles.add(a.getTitle());
 			}
 
-			Log.w("app", String.valueOf(items.getLength()));
+			Log.w("app", "number of articles loaded: " + String.valueOf(items.getLength()));
+
+			ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
+
+			setListAdapter(adapter);
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Log.w("app", "list item clicked");
+		
+		//	   Uri uri = Uri.parse(links.get(position));
+		//	   Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+		//	   startActivity(intent);  
 	}
 
 }
